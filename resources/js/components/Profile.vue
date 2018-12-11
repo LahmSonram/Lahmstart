c9eb5c10f6fa6aa084eca3587b84df302f4ff1e7
Lahm <sonram@pakgon.com>
2018-12-12 00:04:42 +0700

    check file size limit and swal

--- resources/js/components/Profile.vue
+++ resources/js/components/Profile.vue
@@ -246,24 +246,33 @@
         },
         methods:{
             updateInfo(){
+                this.$Progress.start();
                 this.form.put('api/profile/')
                 .then(() => {
 
+                    this.$Progress.finish();
                 })
                 .catch(() => {
-
+                    this.$Progress.fail();
                 });
             },
             updateProfile(e){
                 // console.log('uploading');
                 let file = e.target.files[0];
-                console.log(file);
+                // console.log(file);
                 let reader = new FileReader();
-                reader.onloadend = (file) => {
-                    console.log(reader.result);
-                    this.form.photo = reader.result;
+                if(file['size'] < 2111775){
+                    reader.onloadend = (file) => {
+                        // console.log(reader.result);
+                        this.form.photo = reader.result;
+                    }
+                    reader.readAsDataURL(file);
+                }else{
+                    swal(
+                        "Oops!", "You are uploading a large file",
+                        "error"
+                    )
                 }
-                reader.readAsDataURL(file);
             }
         },
         created(){
