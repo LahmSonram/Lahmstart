95cb6c3d6ce14f3b4bebec3070b92a6374a24dfe
Lahm <sonram@pakgon.com>
2018-12-13 00:32:08 +0700

    Show profile photo with vue

--- resources/js/components/Profile.vue
+++ resources/js/components/Profile.vue
@@ -15,7 +15,7 @@
                         <h5 class="widget-user-desc">Web Designer</h5>
                     </div>
                     <div class="widget-user-image">
-                        <img class="img-circle" src="" alt="User Avatar">
+                        <img class="img-circle" :src="getProfilePhoto()" alt="User Avatar">
                     </div>
                     <div class="card-footer">
                         <div class="row">
@@ -253,6 +253,9 @@
             }
         },
         methods:{
+            getProfilePhoto(){
+                return "img/profile/" + this.form.photo;
+            },
             updateInfo(){
                 this.$Progress.start();
                 this.form.put('api/profile/')
